7cfee184ddcce44a7c512ff47e5da0dac4d5cf71
Ercan Duman <ercan.duman@macellan.net>
2021-02-27 11:19:34 +0300

    Getting data from API is tested and data retrieved from API successfully.

--- app/src/main/java/ercanduman/newsapidemo/data/network/NewsAPI.kt
+++ app/src/main/java/ercanduman/newsapidemo/data/network/NewsAPI.kt
@@ -3,6 +3,7 @@ package ercanduman.newsapidemo.data.network
 import ercanduman.newsapidemo.data.network.model.NewsAPIResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Retrieves data from News API and contains HTTP related functions.
@@ -19,6 +20,9 @@ interface NewsAPI {
      * @return NewsAPIResponse object with retrofit2.Response which then can be handled for success
      * or failure calls.
      */
-    @GET("top-headlines")
-    suspend fun getArticles(): Response<NewsAPIResponse>
+    @GET("everything")
+    suspend fun getArticles(
+        @Query("q") searchQuery: String,
+        @Query("apiKey") apiKey: String
+    ): Response<NewsAPIResponse>
 }
