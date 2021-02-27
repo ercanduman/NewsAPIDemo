ff2fb97beeed4517f0498dcc899e3845c1da2a88
Ercan Duman <ercan.duman@macellan.net>
2021-02-27 11:33:34 +0300

    Hilt DI added to MainApplication.kt to attach lifecycle of application

---
+++ app/src/main/java/ercanduman/newsapidemo/MainApplication.kt
@@ -0,0 +1,16 @@
+package ercanduman.newsapidemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *This generated Hilt component is attached to the Application object's lifecycle and
+ * provides dependencies to it.
+ *
+ *@HiltAndroidApp annotation triggers Hilt's code generation.
+ *
+ * @author ercan
+ * @since  2/27/21
+ */
+@HiltAndroidApp
+class MainApplication : Application()
