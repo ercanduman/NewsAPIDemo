152950ca7ba1e74850d5ef11f590a940e49795b4
Ercan Duman <ercan.duman@macellan.net>
2021-02-28 22:55:25 +0300

    DetailsFragment.kt created to display article in WebView

---
+++ app/src/main/java/ercanduman/newsapidemo/ui/main/detail/DetailsFragment.kt
@@ -0,0 +1,11 @@
+package ercanduman.newsapidemo.ui.main.detail
+
+import androidx.fragment.app.Fragment
+import ercanduman.newsapidemo.R
+
+/**
+ * Display passed article in WebView.
+ *
+ * FAB button in this fragment will be used to bookmark an article that will be save locally.
+ */
+class DetailsFragment : Fragment(R.layout.fragment_details)
