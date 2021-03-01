8d12331c9fe3db1a0ca6c0966563eff74937bf59
Ercan Duman <ercan.duman@macellan.net>
2021-03-01 09:05:20 +0300

    Separated function created for searching articles from API

--- app/src/main/java/ercanduman/newsapidemo/data/network/response/NewsAPIResponse.kt
+++ app/src/main/java/ercanduman/newsapidemo/data/network/response/NewsAPIResponse.kt
@@ -6,6 +6,9 @@ import ercanduman.newsapidemo.data.network.model.Article
 /**
  * Stores all JSON content retrieved from API call. GSON library is responsible to parse JSON and
  * set to class members.
+ *
+ * This class contains list of Article objects, status of network call and total result count
+ * that returned from api.
  */
 data class NewsAPIResponse(
     @SerializedName("articles")
