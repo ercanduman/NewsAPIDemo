62caeb9c7d54483dbe4ca6fc0ba5f8ab97720130
Ercan Duman <ercan.duman@macellan.net>
2021-03-01 10:13:05 +0300

    Retrieving locally saved data function added to ArticleDao.kt with Flow API

--- app/src/main/java/ercanduman/newsapidemo/data/db/ArticleDao.kt
+++ app/src/main/java/ercanduman/newsapidemo/data/db/ArticleDao.kt
@@ -1,16 +1,38 @@
 package ercanduman.newsapidemo.data.db
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
 import ercanduman.newsapidemo.data.network.model.Article
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access object that stores all SqLite queries for database interactions.
  *
  * At compile time, Room will generate an implementation of this class when it is referenced
  * by a Database.
+ *
+ *
+ * For retrieving data Kotlin Flow API is used.
+ *
+ * Flow: An asynchronous data stream that sequentially emits values and completes
+ * normally or with an exception. Execution of the flow is also called collecting the data
+ * which is always performed in a suspending manner without actual blocking.
+ *
+ * Flow API in Kotlin is a better way to handle the stream of data asynchronously
+ * that executes sequentially.
+ *
+ *   " Key Difference between LiveData & Kotlin Flow
+ *   LiveData is used to observe data without having any hazel to handle lifecycle problems.
+ *   Whereas Kotlin flow is used for continuous data integration and it also simplified the asynchronous programming.
+ *   Take Room Library as an example. First, it used LiveData to transmit data from the database to UI.
+ *   It solved most of the existing problems. But when there are any future changes in the database LiveData is helpless in this situation.
+ *   After a while, the room used Kotlin flow to solve this problem.
+ *   With Flow as return-type, room created a new possibility of seamless data integration across
+ *   the app between database and UI without writing any extra code.
+ *
+ *   For more details: https://medium.com/android-dev-hacks/exploring-livedata-and-kotlin-flow-7c8d8e706324 "
+ *
+ * As a result, Flow will be used for interactions with Room library, and LiveData (which is lifecycle aware)
+ * will be used inside ViewModel and UI related components for observing data changes.
  *
  * @author ercan
  * @since  3/1/21
@@ -32,4 +54,10 @@ interface ArticleDao {
      */
     @Delete
     suspend fun delete(article: Article)
+
+    /**
+     * Retrieves all locally saved articles and returns flow of list of them.
+     */
+    @Query("SELECT * FROM article")
+    fun getSavedArticles(): Flow<List<Article>>
 }
