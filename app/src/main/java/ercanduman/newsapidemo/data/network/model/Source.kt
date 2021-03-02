19831b2e9f99247d86679e3020a5822f422eaa4d
Ercan Duman <ercan.duman@macellan.net>
2021-03-02 10:08:53 +0300

    Parcelable implementation added to Article.kt for sending with bundle

--- app/src/main/java/ercanduman/newsapidemo/data/network/model/Source.kt
+++ app/src/main/java/ercanduman/newsapidemo/data/network/model/Source.kt
@@ -1,6 +1,10 @@
 package ercanduman.newsapidemo.data.network.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Source(
     val id: String,
     val name: String
-)
+) : Parcelable
