6d55ce87bb8f5a5a21811e624fe570514c5c253b
Ercan Duman <ercan.duman@macellan.net>
2021-03-03 15:13:37 +0300

    Unnecessary resources removed

--- app/src/androidTest/java/ercanduman/newsapidemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ercanduman/newsapidemo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ercanduman.newsapidemo", appContext.packageName)
+        assertEquals("ercanduman.newsapidemo.debug", appContext.packageName)
     }
 }
