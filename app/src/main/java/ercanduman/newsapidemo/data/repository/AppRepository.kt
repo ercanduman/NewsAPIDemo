4c98e3b116dcbbef66dba0d207ca9bcd5c93ec42
Ercan Duman <ercan.duman@macellan.net>
2021-03-04 15:02:25 +0300

    AppRepository.kt code documentation improved

--- app/src/main/java/ercanduman/newsapidemo/data/repository/AppRepository.kt
+++ app/src/main/java/ercanduman/newsapidemo/data/repository/AppRepository.kt
@@ -7,15 +7,27 @@ import ercanduman.newsapidemo.data.network.response.NewsAPIResponse
 import ercanduman.newsapidemo.util.ApiEvent
 import retrofit2.Response
 import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
  * Abstraction layer which is a mediator and responsible to retrieve data from different data
- * sources and provides data for ViewModel.
- * Different data sources means Room database for caching or Retrofit for remote networking data.
+ * sources and provides data for ViewModel. For example, different data sources can be Room database
+ * for caching or Retrofit for remote networking data.
+ *
+ * Annotating with @Inject means this constructor is injectable and accept zero or more dependencies
+ * as arguments. @Inject can apply to at most one constructor per class.
+ *
+ * Here "Constructor Injection" used instead of "Field Injection". Because when field injection used,
+ * later on it cannot be clear what objects injected in Repository and classes will be tightly
+ * coupled. For more drawbacks of field Injection: https://stackoverflow.com/a/39892204/4308897
+ *
+ * After making the repository as @Singleton and inject it in ViewModels, any ViewModel now has
+ * access to that specific repository.
  *
  * @author ercanduman
  * @since  27.02.2021
  */
+@Singleton
 class AppRepository @Inject constructor(private val api: NewsAPI, private val dao: ArticleDao) {
 
     /**
