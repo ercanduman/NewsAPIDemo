44cb0399e36f95d6d7520067175dde51eb69e253
Ercan Duman <ercan.duman@macellan.net>
2021-03-12 09:35:30 +0300

    SavedStateHandle added to NewsViewModel.kt to store and retrieve search query

--- app/src/main/java/ercanduman/newsapidemo/ui/main/news/NewsViewModel.kt
+++ app/src/main/java/ercanduman/newsapidemo/ui/main/news/NewsViewModel.kt
@@ -1,14 +1,12 @@
 package ercanduman.newsapidemo.ui.main.news
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ercanduman.newsapidemo.data.network.model.Article
 import ercanduman.newsapidemo.data.repository.AppRepository
+import ercanduman.newsapidemo.util.log
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -25,14 +23,33 @@ import javax.inject.Inject
  * NewsViewModel retrieves Breaking news and searched articles from [AppRepository] and hold it for
  * UI components.
  *
+ * SavedStateHandle ia a handle to saved state passed to ViewModel. This is a key-value map that
+ * will let you write and retrieve objects to and from the saved state. These values will persist
+ * after the process is killed by the system and remain available via the same object.
+ *
+ * Annotation of Assisted is a Dagger annotation which uses SavedStateViewModelFactory to receive &
+ * inject this object in ViewModel's constructor.
+ *
  * @author ercanduman
  * @since  25.02.2021
  */
 @HiltViewModel
-class NewsViewModel @Inject constructor(private val repository: AppRepository) : ViewModel() {
+class NewsViewModel @Inject constructor(
+    private val repository: AppRepository,
+    state: SavedStateHandle
+) : ViewModel() {
 
     private val _articles = MutableLiveData<PagingData<Article>>()
     val articles: LiveData<PagingData<Article>> = _articles
+
+    val currentQuery: MutableLiveData<String> = state.getLiveData(KEY_CURRENT_QUERY, "")
+
+    init {
+        currentQuery.observeForever { newQuery ->
+            log("observeForever called for query: $newQuery")
+            getAllArticles(newQuery)
+        }
+    }
 
     /**
      * Loads data for query parameter and passed new data to [_articles] mutable live data.
@@ -41,22 +58,26 @@ class NewsViewModel @Inject constructor(private val repository: AppRepository) :
      * To avoid memory leaks, make sure to use a scope that is already managed (like a ViewModel
      * scope) or manually cancel it when you don't need paging anymore.
      */
-    fun searchArticlesPaging(query: String) = viewModelScope.launch {
-        repository.searchArticlesPagination(query)
-            .cachedIn(viewModelScope)
-            .collect { _articles.value = it }
+    fun searchArticlesPaging(query: String) {
+        currentQuery.value = query
     }
 
     /**
-     * Retrieves Breaking news by communicating to AppRepository
+     * Retrieves Breaking news and searched news by communicating to AppRepository
      *
      * cachedIn: The flow is kept active as long as the given scope is active.
      * To avoid memory leaks, make sure to use a scope that is already managed (like a ViewModel
      * scope) or manually cancel it when you don't need paging anymore.
      */
-    fun getBreakingNewsArticles() = viewModelScope.launch {
-        repository.getArticles().cachedIn(viewModelScope).collect {
-            _articles.value = it
+    private fun getAllArticles(newQuery: String) = viewModelScope.launch {
+        if (newQuery.isNotEmpty()) {
+            repository.searchArticles(newQuery).cachedIn(viewModelScope).collect {
+                _articles.value = it
+            }
+        } else {
+            repository.getBreakingNewsArticles().cachedIn(viewModelScope).collect {
+                _articles.value = it
+            }
         }
     }
 
@@ -65,5 +86,9 @@ class NewsViewModel @Inject constructor(private val repository: AppRepository) :
      */
     fun saveArticleClicked(article: Article) = viewModelScope.launch {
         repository.insert(article.copy(isSaved = true))
+    }
+
+    companion object {
+        private const val KEY_CURRENT_QUERY = "key_current_query"
     }
 }
